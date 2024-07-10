87f2130860a45da2885d33d8772cd9bda4b6b565
Sharon Wendoh <75203077+SharonWendoh@users.noreply.github.com>
2024-07-10 07:38:43 +0300

    Update side-bar.tsx

---
+++ src/app/components/app-bars/side-bar.tsx
@@ -0,0 +1,116 @@
+"use client"
+import * as React from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { useMediaQuery } from '@mui/material';
+import NavLinks from './nav-links';
+
+const drawerWidth = 240;
+
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+interface SideNavProps {
+  open: boolean,
+  handleDrawerOpen: () => void,
+  handleDrawerClose: () => void,
+}
+
+export default function SideNav(
+  { open, handleDrawerOpen, handleDrawerClose }: SideNavProps) {
+  const theme = useTheme();
+  const mobileCheck = useMediaQuery(theme.breakpoints.up('sm'))
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer 
+      variant="permanent" 
+      open={open}
+      anchor='left'
+      sx={{
+        ['& .MuiDrawer-paper']: {
+          top: mobileCheck ? 64 :57
+        },
+      }}
+       >
+        <DrawerHeader>
+          {!open ? (
+            <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 1.4,
+              display: open ? 'none' : 'inline-flex',
+            }}
+          >
+            <ChevronRightIcon />
+          </IconButton>
+          ) : (
+            <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+          )}
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <NavLinks open={open} />
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
